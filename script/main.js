317b722dbdd9df8b872ddb15a6c515175a9bfbff
mostofapro <mostofathegreat@gmail.com>
2025-03-02 22:43:32 +0600

    some function done

--- script/main.js
+++ script/main.js
@@ -1,11 +1,12 @@
 
+// Random Color Change
 document.getElementById("theme-btn").addEventListener("click", function () {
   const randomColor = "#" + Math.floor(Math.random() * 16777215).toString(16);
   document.body.style.backgroundColor = randomColor;
 });
 
 // Date Area
-function formatDate() {
+function currentDate() {
   const date = new Date();
   const options = { weekday: 'short', month: 'short', day: 'numeric', year: 'numeric' };
   const formatted = date.toLocaleDateString('en-US', options).replace(',', '');
@@ -16,4 +17,11 @@ function formatDate() {
   document.getElementById("fullDate").innerText = `${month} ${day} ${year}`;
 }
 
-formatDate();
+currentDate();
+
+// To the Blog Page
+
+document.getElementById('discover-new').addEventListener("click", function(event){
+  event.preventDefault();
+  window.location.href = "./blog.html"
+})
